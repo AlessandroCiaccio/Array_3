8430690cb43b83697c305380633e9d82b4900f38
Alessandro <sandroscro@libero.it>
2023-10-17 15:32:29 +0200

    Aggiunto metodo

--- MyClass.java
+++ MyClass.java
@@ -1,29 +1,29 @@
 public class MyClass {
     public static void main(String args[]) {
-       int[] firstArr = {1, 2, 2, 3, 4, 4, 5};
-       
-       int[] secondArr = new int[firstArr.length];
+       int[] firstArr = {1, 2, 2, 3, 4, 4, 5, 5, 3, 2, 2, 3, 6, 7, 6, 4};
+       togliDuplicati(firstArr);
+
+    }
+    public static void togliDuplicati(int[] array){
+        int[] copia = new int[array.length];
         int ind=0;
-        for (int i=0; i<firstArr.length; i++){
+        for (int i=0; i<array.length; i++){
             boolean isDup=false;
             for (int j=0; j<ind; j++){
-                if(firstArr[i]==secondArr[j]){
+                if(array[i]==copia[j]){
                     isDup=true;
                     break;
                 }
             }
             if(!isDup){
-                secondArr[ind]=firstArr[i];
+                copia[ind]=array[i];
                 ind++;
             }
         }
-       
-       
-       
-       for (int i : secondArr) {
-            if (i != 0) { 
-                System.out.print(i + " ");
-            }
+        int[] arr=new int[ind];
+        for(int i=0; i<arr.length; i++){
+            arr[i]=copia[i];
+            System.out.print(arr[i]+"  ");
         }
     }
 }
